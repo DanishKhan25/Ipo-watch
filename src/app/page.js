a1006d3a9227c7411b32e0dad579b516cb74173d
Danish_khan <danishkhan25nov@gmail.com>
2024-02-28 11:37:17 +0530

    added some styles

--- src/app/page.js
+++ src/app/page.js
@@ -27,19 +27,22 @@ function IPOList() {
   }, []);
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold mb-4">Listed IPOs</h1>
-      <table className="border-collapse border border-black">
+    <div className="container mx-auto ">
+      <h1 className="text-3xl font-bold mb-4 text-center mt-10">Listed IPOs</h1>
+      <table className="border-collapse border border-black w-full bg-gray-100">
         <thead>
           <tr>
             <th className="border border-black px-4 py-2">Company Name</th>
-            <th className="border border-black px-4 py-2 max-w-200">URL</th>
+            <th className="border border-black px-4 py-2">URL</th>
             <th className="border border-black px-4 py-2">Symbol</th>
             <th className="border border-black px-4 py-2">Industry</th>
             <th className="border border-black px-4 py-2">IPO Type</th>
+            <th className="border border-black px-4 py-2">QIB Sub</th>
             <th className="border border-black px-4 py-2">Listing Date</th>
             <th className="border border-black px-4 py-2">Issue Price</th>
             <th className="border border-black px-4 py-2">Last Price</th>
+            <th className="border border-black px-4 py-2">Listing Open</th>
+            <th className="border border-black px-4 py-2">Listing Close</th>
           </tr>
         </thead>
         <tbody>
@@ -48,8 +51,16 @@ function IPOList() {
               <td className="border border-black px-4 py-2">
                 {ipo.company_name || "N/A"}
               </td>
-              <td className="border border-black px-4 py-2 max-w-200">
-                <a href={ipo.url} target="_blank" rel="noopener noreferrer">
+              <td
+                className="border border-black px-4 py-2 max-w-32 text-blue text-ellipsis overflow-hidden"
+                style={{ wordWrap: "break-word" }}
+              >
+                <a
+                  className="max-w-32 overflow-hidden text-ellipsis text-blue"
+                  href={ipo.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   {ipo.url || "N/A"}
                 </a>
               </td>
@@ -62,6 +73,13 @@ function IPOList() {
               <td className="border border-black px-4 py-2">
                 {ipo.ipo_type || "N/A"}
               </td>
+              <td
+                className={`border border-black px-4 py-2 ${
+                  ipo.total_subs > 25 ? "text-green-500" : "text-black"
+                }`}
+              >
+                {ipo.total_subs || "N/A"}
+              </td>
               <td className="border border-black px-4 py-2">
                 {ipo.listing_date || "N/A"}
               </td>
@@ -70,6 +88,12 @@ function IPOList() {
               </td>
               <td className="border border-black px-4 py-2">
                 {ipo.last_price || "N/A"}
+              </td>
+              <td className="border border-black px-4 py-2">
+                {ipo.dt_open || "N/A"}
+              </td>
+              <td className="border border-black px-4 py-2">
+                {ipo.dt_close || "N/A"}
               </td>
             </tr>
           ))}
